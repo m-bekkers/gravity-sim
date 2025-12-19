da9a8d0d33323c392bb6ea1dce3fdde5d5fbc998
m-bekkers <matthedeb@hotmail.com>
2025-12-19 11:32:36 -0500

    small fix

--- common/loadShader.h
+++ common/loadShader.h
@@ -8,6 +8,6 @@
 #include <GL/glew.h>
 
 // note: this function was originally in cpp
-GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path)
+GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
 
 #endif
